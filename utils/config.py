d2bacf133168e2f78eb343da82a2216befb3cd4b
Rafael Ziani <rafael.ziani1@gmail.com>
2026-08-30 16:49:52 -0300

    Update config.py: configure dynamic paths for regional lexicons and checkpoints

--- utils/config.py
+++ utils/config.py
@@ -8,10 +8,14 @@ gpu = torch.device('cuda')
 
 
 class DataConfig(object):
-    language = os.getenv('LANGUAGE', 'PT-IPA')
-    graphemes_path = f'resources/{language}/Graphemes.json'
-    phonemes_path = f'resources/{language}/Phonemes.json'
-    lexicon_path = f'resources/{language}/Lexicon.json'
+    language = os.getenv('LANGUAGE', 'spx')
+    
+    # Caminhos globais compartilhados
+    graphemes_path = 'resources/global/Graphemes.json'
+    phonemes_path = 'resources/global/Phonemes.json'
+    
+    # Caminho dinâmico para o léxico da região ativa
+    lexicon_path = f'resources/lexicons/{language}.json'
 
 
 class ModelConfig(object):
@@ -29,10 +33,12 @@ class TrainConfig(object):
     lr = 2e-4
     batch_size = 64
     epochs = int(os.getenv('EPOCHS', '30'))
+    # Log organizado por sotaque/região (ex: log/spx)
     log_path = f'log/{DataConfig.language}'
 
 
 class TestConfig(object):
     device = cpu
-    encoder_model_path = f'checkpoints/encoder_e{TrainConfig.epochs:02}.pth'
-    decoder_model_path = f'checkpoints/decoder_e{TrainConfig.epochs:02}.pth'
+    # Checkpoints organizados por sotaque/região (ex: checkpoints/spx/encoder_e30.pth)
+    encoder_model_path = f'checkpoints/{DataConfig.language}/encoder_e{TrainConfig.epochs:02}.pth'
+    decoder_model_path = f'checkpoints/{DataConfig.language}/decoder_e{TrainConfig.epochs:02}.pth'
