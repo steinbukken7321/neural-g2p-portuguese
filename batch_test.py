b5a212ed32abe479b88e530c9f486a0cc89ef8fb
Rafael Ziani <rafael.ziani1@gmail.com>
2026-08-31 08:55:14 -0300

    Fix batch_test.py: add utf-8 encoding for list file I/O

--- batch_test.py
+++ batch_test.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
-
 import os
 import argparse
+
+parser = argparse.ArgumentParser(description='Teste em lote do modelo G2P por sotaque/região.')
+parser.add_argument('--sotaque', type=str, default=None,
+                     help='Código do sotaque/região para teste (ex: spx, rjx)')
+parser.add_argument('--visualize', action='store_true')
+parser.add_argument('--list_path', type=str, default='list.txt')
+parser.add_argument('--output_path', type=str, default='new_ipa.txt')
+args = parser.parse_args()
+
+if args.sotaque:
+    os.environ['SOTAQUE'] = args.sotaque
 
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-from data import PersianLexicon
+from utils.data import ParserLexicon
 from model import Encoder, Decoder
-from config import DataConfig, ModelConfig, TestConfig
+from utils.config import DataConfig, ModelConfig, TestConfig
 
 
 def load_model(model_path, model):
     model.load_state_dict(torch.load(
-        model_path,
-        map_location=lambda storage,
-        loc: storage
+        model_path, map_location=lambda storage, loc: storage
     ))
     model.to(TestConfig.device)
     model.eval()
@@ -25,24 +33,14 @@ def load_model(model_path, model):
 
 class G2P(object):
     def __init__(self):
-        # data
-        self.ds = PersianLexicon(
+        self.ds = ParserLexicon(
             DataConfig.graphemes_path,
             DataConfig.phonemes_path,
             DataConfig.lexicon_path
         )
-
-        # model
-        self.encoder_model = Encoder(
-            ModelConfig.graphemes_size,
-            ModelConfig.hidden_size
-        )
+        self.encoder_model = Encoder(ModelConfig.graphemes_size, ModelConfig.hidden_size)
         load_model(TestConfig.encoder_model_path, self.encoder_model)
-
-        self.decoder_model = Decoder(
-            ModelConfig.phonemes_size,
-            ModelConfig.hidden_size
-        )
+        self.decoder_model = Decoder(ModelConfig.phonemes_size, ModelConfig.hidden_size)
         load_model(TestConfig.decoder_model_path, self.decoder_model)
 
     def __call__(self, word, visualize):
@@ -50,72 +48,46 @@ class G2P(object):
         x = torch.tensor(x).long().unsqueeze(1)
         with torch.no_grad():
             enc = self.encoder_model(x)
-
         phonemes, att_weights = [], []
         x = torch.zeros(1, 1).long().to(TestConfig.device)
-        hidden = torch.ones(
-            1,
-            1,
-            ModelConfig.hidden_size
-        ).to(TestConfig.device)
-        t = 0
+        hidden = torch.ones(1, 1, ModelConfig.hidden_size).to(TestConfig.device)
         while True:
             with torch.no_grad():
-                out, hidden, att_weight = self.decoder_model(
-                    x,
-                    enc,
-                    hidden
-                )
-
+                out, hidden, att_weight = self.decoder_model(x, enc, hidden)
             att_weights.append(att_weight.detach().cpu())
             max_index = out[0, 0].argmax()
             x = max_index.unsqueeze(0).unsqueeze(0)
-            t += 1
-
             phonemes.append(self.ds.idx2p[max_index.item()])
             if max_index.item() == 1:
                 break
-
         if visualize:
             att_weights = torch.cat(att_weights).squeeze(1).numpy().T
             y, x = att_weights.shape
             plt.imshow(att_weights, cmap='gray')
             plt.yticks(range(y), ['<sos>'] + list(word) + ['<eos>'])
             plt.xticks(range(x), phonemes)
-            plt.savefig(f'attention/{DataConfig.language}/{word}.png')
-
+            os.makedirs(f'attention/{DataConfig.sotaque}', exist_ok=True)
+            plt.savefig(f'attention/{DataConfig.sotaque}/{word}.png')
         return phonemes
 
+
 def get_list(path):
-    new_l = []
-    with open(path,'r') as list_file:
-        for row in list_file:
-            if row:
-                new_l.append(row.strip())
-    return new_l
-    
-def from_list_file(list_phone,file_name):
-    
-    new_f = open(file_name,"w+")
-    for item in list_phone:
-        new_f.write(item+'\n')
-    new_f.close()
+    with open(path, 'r', encoding='utf-8') as list_file:
+        return [row.strip() for row in list_file if row]
+
+
+def from_list_file(list_phone, file_name):
+    with open(file_name, "w+", encoding='utf-8') as new_f:
+        for item in list_phone:
+            new_f.write(item + '\n')
+
 
 if __name__ == '__main__':
-    # get word
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--word', type=str, default='testando')
-    parser.add_argument('--visualize', action='store_true')
-    args = parser.parse_args()
-    
-    l = get_list('list.txt')
+    l = get_list(args.list_path)
     n_l = []
-
     g2p = G2P()
-    
     for item in l:
         result = g2p(item, args.visualize)
-        print(item+"  "+" ".join(result[:-1]))
-        n_l.append(item+"  "+" ".join(result[:-1])) 
-
-    from_list_file(n_l,"new_ipa.txt")
+        print(item + "  " + " ".join(result[:-1]))
+        n_l.append(item + "  " + " ".join(result[:-1]))
+    from_list_file(n_l, args.output_path)
