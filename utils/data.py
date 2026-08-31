4f0adc2d19b693e6d9ae4c57038381ea9354deaa
Rafael Ziani <rafael.ziani1@gmail.com>
2026-08-31 08:59:18 -0300

    Fix utils/data.py: add utf-8 encoding when parsing lexicon files

--- utils/data.py
+++ utils/data.py
@@ -9,14 +9,21 @@ from torch.utils.data import Dataset, DataLoader
 class ParserLexicon(Dataset):
     def __init__(self, inputs, outputs, dict_path):
         with open(inputs, encoding='utf-8') as fi, open(outputs, encoding='utf-8') as fo, open(dict_path, encoding='utf-8') as fd:
-                    graphemes = json.load(fi)
-                    phonemes = json.load(fo)
-                    self.lexicon = json.load(fd)
+            graphemes = json.load(fi)
+            phonemes = json.load(fo)
+            self.lexicon = json.load(fd)
 
+        # Mapeamentos originais
         self.g2idx = {ch: idx for idx, ch in enumerate(graphemes)}
         self.idx2g = {idx: ch for idx, ch in enumerate(graphemes)}
         self.p2idx = {phn: idx for idx, phn in enumerate(phonemes)}
         self.idx2p = {idx: phn for idx, phn in enumerate(phonemes)}
+
+        # Aliases para manter compatibilidade com o inference.py original
+        self.graphemes2idx = self.g2idx
+        self.idx2phonemes = self.idx2p
+        self.sos_idx = 0
+        self.eos_idx = 1
 
     def __len__(self):
         return len(self.lexicon)
@@ -24,19 +31,14 @@ class ParserLexicon(Dataset):
     def __getitem__(self, index):
         key, value = self.lexicon[index]
         
-        x = 0
-        y = 0
-        ch = 0
         try:
             x = [self.g2idx[ch] for ch in key]
             y = [self.p2idx[phn] for phn in value.split(' ') if phn != '']
         except Exception as e:
-            print("key",key)
+            print("key", key)
             raise e
         
-        
-
-        return [0] + x + [1], [0] + y + [1]
+        return [self.sos_idx] + x + [self.eos_idx], [self.sos_idx] + y + [self.eos_idx]
 
 
 def collate_fn(batch):
